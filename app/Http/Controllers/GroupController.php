9ebedf6e9041b57ae2099006e640415d8765dba4
plump_albert <plumpalbert@gmail.com>
2022-01-05 17:21:59 +0300

    Created controller for group route

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class GroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $group = null;
+        if ($request->has('id')) {
+            $group = Group::find($request->id);
+        } else {
+            $group = Group::all();
+        }
+        $response = null;
+        if (!$group) {
+            $response = new Response([
+                'error' => true,
+                'message' => 'Group not found',
+                'body' => null
+            ], 404);
+        } else {
+            $response = new Response([
+                'error' => false,
+                'message' => '',
+                'body' => $group
+            ]);
+        }
+        return $response;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'faculty' => 'required|string',
+            'specialty' => 'required|string',
+            'year' => 'required|integer',
+        ]);
+        $group = Group::create($request->all(['faculty', 'specialty', 'year']));
+        return Response([
+            'error' => false,
+            'message' => 'Group successfully created',
+            'body' => $group
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

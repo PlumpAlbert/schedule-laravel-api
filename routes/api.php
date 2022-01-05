a0a34469f8813d9c4a57ecd00ff805a88fa098e4
plump_albert <plumpalbert@gmail.com>
2022-01-05 17:21:34 +0300

    Created endpoints for `UserController`

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('subject', [SubjectController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/delete', [UserController::class, 'delete']);
+});
